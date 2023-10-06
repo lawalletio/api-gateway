e023174d42b67c0f763235ef080fc45aecda63ce
Juan Ramírez <artjjrn@gmail.com>
2023-10-06 11:41:07 -0300

    Remove unnecesary log

--- src/rest/lnurlp/:pubkey/callback/get.ts
+++ src/rest/lnurlp/:pubkey/callback/get.ts
@@ -2,9 +2,6 @@ import { Response } from 'express';
 import http from 'http';
 
 import type { ExtendedRequest } from '@type/request';
-import { logger } from '@lib/utils';
-
-const log: debug.Debugger = logger.extend('rest:nostr:publish:post');
 
 const handler = (req: ExtendedRequest, res: Response) => {
   http.get(`http://localhost:3001${req.url}`, (response) => {
