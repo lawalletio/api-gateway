43e2163065009afc41bc55c17d6e00fadc18d523
Juan Ramírez <artjjrn@gmail.com>
2023-10-06 17:58:54 -0300

    Get urlx uri from env variables

--- src/rest/lnurlp/:pubkey/callback/get.ts
+++ src/rest/lnurlp/:pubkey/callback/get.ts
@@ -2,9 +2,10 @@ import { Response } from 'express';
 import http from 'http';
 
 import type { ExtendedRequest } from '@type/request';
+import { requiredEnvVar } from '@lib/utils';
 
 const handler = (req: ExtendedRequest, res: Response) => {
-  http.get(`http://localhost:3001${req.url}`, (response) => {
+  http.get(`${requiredEnvVar('URLX_URI')}${req.url}`, (response) => {
     response.on('data', (data) => res.write(data));
     response.on('end', () => res.end());
   });
