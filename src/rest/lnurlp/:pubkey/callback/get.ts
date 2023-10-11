e72e26996ddfc1a3f19a4b964d012e2e9700cad0
Juan Ramírez <artjjrn@gmail.com>
2023-10-11 11:23:55 -0300

    Passthrough status code on urlx request

--- src/rest/lnurlp/:pubkey/callback/get.ts
+++ src/rest/lnurlp/:pubkey/callback/get.ts
@@ -2,14 +2,18 @@ import { Response } from 'express';
 import http from 'http';
 
 import type { ExtendedRequest } from '@type/request';
-import { requiredEnvVar } from '@lib/utils';
+import { logger, requiredEnvVar } from '@lib/utils';
+
+const log: debug.Debugger = logger.extend('rest:lnurlp:pubkey:callback:get');
+const error: debug.Debugger = log.extend('error');
 
 const handler = (req: ExtendedRequest, res: Response) => {
   http.get(`${requiredEnvVar('URLX_URI')}${req.url}`, (response) => {
     response.on('data', (data) => res.write(data));
-    response.on('end', () => {
-      res.status(response.statusCode || 503).end();
-    });
+    response.on('end', () => res.status(response.statusCode || 503).end());
+  }).on('error', (e) => {
+    error('Unexpected error: %O', e);
+    res.status(500).send()
   });
 };
 
